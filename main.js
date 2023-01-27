72df8d0bf7ed4c72e4c05ba4c73009a4147e1f3e
tomohiro <96575891+tomohiro47@users.noreply.github.com>
2023-01-27 12:30:26 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+let a
+console.log(a != 10);
+
+let b
+console.log(b >= 10 && b < 20);
+
+let x = 10;
+
+if (x >= 10 && x <= 20) {
+  console.log('成功です');
+} else {
+  console.log('失敗です');  
+}
+
+let y = 0;
+
+if (y% 2 === 0) {
+  console.log('偶数です');
+} 
+if (y % 2 !== 0) {
+  console.log('奇数です');
+}
